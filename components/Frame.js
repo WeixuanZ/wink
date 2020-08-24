8c38b2f13c06335abdd27617994c11feab307f74
W Zhang <wz315@cam.ac.uk>
2020-08-24 10:43:24 +0100

    feat: Smooth scrolling
    Achieved by injecting [iamdustan/smoothscroll](https://github.com/iamdustan/smoothscroll) on load.

--- components/Frame.js
+++ components/Frame.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from 'react'
 import { View, ActivityIndicator, StyleSheet } from 'react-native'
-import ProgressWebView from 'react-native-progress-webview';
+import ProgressWebView from 'react-native-progress-webview'
 
-export default function Frame ({currentUrl, webviewRef, handleStateChange}) {
+import { smoothScroll } from '../lib/scroll.js'
+
+export default function Frame({ currentUrl, webviewRef, handleStateChange }) {
   return (
     <View style={styles.container}>
       <ProgressWebView
         source={{ uri: currentUrl }}
         ref={webviewRef}
         onNavigationStateChange={handleStateChange}
+        onLoad={() => webviewRef.current.injectJavaScript(smoothScroll)}
         allowsFullscreenVideo={true}
         allowsInlineMediaPlayback={true}
         allowsBackForwardNavigationGestures={true}
         startInLoadingState={true}
         renderLoading={() => (
           <ActivityIndicator
-            color='black'
-            size='large'
+            color="black"
+            size="large"
             style={styles.container}
           />
         )}
