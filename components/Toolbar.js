acb7e50b968d724159207f64c10abbf62264d47a
W Zhang <wz315@cam.ac.uk>
2020-08-31 17:31:26 +0100

    fix: Winks or blinks affect typing
    Winks or blinks will be ignored when keyboard is visible.

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, TouchableOpacity, Keyboard, StyleSheet } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { Camera } from 'expo-camera'
 import * as Haptics from 'expo-haptics'
@@ -11,22 +11,44 @@ import { useStoredState } from '../lib/storage.js'
 import faceAction from '../lib/face.js'
 import colors, { faceRecBtnColors } from '../config/colors.js'
 
-export default function Toolbar({ seachbarFocused, searchbarRef, webviewRef, ...props }) {
+export default function Toolbar({
+  seachbarFocused,
+  searchbarRef,
+  webviewRef,
+  ...props
+}) {
   // true|false: whether face detection is enabled
-  const [faceTrackState, setFaceTrackState] = useStoredState('@facetrack_state', true)
+  const [faceTrackState, setFaceTrackState] = useStoredState(
+    '@facetrack_state',
+    true
+  )
   // true|false: whether have camera permission
   const [permissionGranted, setPermissionGranted] = useState(false)
   // 'noPermission'|'noFace'|'normal': the state of face detection
   const [faceState, setFaceState] = useState('noFace')
 
-  // check camera permission
+  const [keyboardVisible, setKeyboardVisible] = useState(false)
+
   useEffect(() => {
     (async () => {
+      // check camera permission
       const { status } = await Camera.requestPermissionsAsync()
       setPermissionGranted(status === 'granted')
       setFaceState(status === 'granted' ? 'noFace' : 'noPermission')
-      searchbarRef.current.focus()
+      searchbarRef.current.focus() // prevent focusing searchbar from affecting permission requests
     })()
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => setKeyboardVisible(true)
+    )
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => setKeyboardVisible(false)
+    )
+    return () => {
+      keyboardDidShowListener.remove()
+      keyboardDidHideListener.remove()
+    }
   }, [])
 
   return (
@@ -74,7 +96,8 @@ export default function Toolbar({ seachbarFocused, searchbarRef, webviewRef, ...
             setFaceState('noFace')
             return
           }
-          if (seachbarFocused) {
+          if (seachbarFocused || keyboardVisible) {
+            // disable when the searchbar is focused or keyboard is visible
             return
           }
           faceAction(faces[0], setFaceState, webviewRef, searchbarRef)
